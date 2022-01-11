eeedf4b6c0b3e3d083453752a72ac3a2912dedcb
HanMinSS <gksals6464@naver.com>
2022-01-11 15:38:32 +0900

    #17 ✅  로그인 페이지 이메일 정규표현식 확인 추가

--- src/js/login.js
+++ src/js/login.js
@@ -1,15 +1,20 @@
 let email = document.querySelector("#email");
 let pwd = document.querySelector("#pwd");
 
-console.log(document.cookie);
 const handleCheckInput = () => {
-  if (!!email.value && !!pwd.value) {
+  if (!!email.value && !!pwd.value && checkEmailFormet()) {
     submitBtn.removeAttribute("disabled");
     submitBtn.className = "btn_submit activate";
   } else {
     submitBtn.setAttribute("disabled", true);
     submitBtn.className = "btn_submit";
   }
+};
+
+const checkEmailFormet = () => {
+  const regExp =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+  return regExp.test(email.value);
 };
 
 email.addEventListener("input", handleCheckInput);

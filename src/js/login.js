a97408332e0de1817fe4b4d215972239d76b6e13
HanMinSS <gksals6464@naver.com>
2022-01-11 17:30:10 +0900

    #17 ♻️  로그인,회원가입 관련 코드정리, 주석 생성, 코드 간결화

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,15 @@
 let email = document.querySelector("#email");
 let pwd = document.querySelector("#pwd");
 let err_msg = document.querySelector("#emailError");
+// email 정규표현식 test
+const checkEmailFormet = () => {
+  const regExp =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+  return regExp.test(email.value);
+};
 
 const handleCheckInput = () => {
+  // email과 pwd를 입력했고 eamil 형식이 부합할 경우 button 활성화
   if (!!email.value && !!pwd.value && checkEmailFormet()) {
     submitBtn.removeAttribute("disabled");
     submitBtn.className = "btn_submit activate";
@@ -12,12 +19,7 @@ const handleCheckInput = () => {
   }
 };
 
-const checkEmailFormet = () => {
-  const regExp =
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-  return regExp.test(email.value);
-};
-
+// 버튼이 활성화되면 버튼에 생기는 onClick event
 const handleOnSubmit = () => {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
@@ -39,10 +41,12 @@ const handleOnSubmit = () => {
   fetch("http://146.56.183.55:5050/user/login", requestOptions)
     .then((response) => response.json())
     .then((result) => {
+      // 로그인에 실패하면 message가 들어있고 성공하면 없다.
       if (result.message) {
         err_msg.innerHTML = "*" + result.message;
       } else {
         err_msg.innerHTML = "";
+        //로그인에 성공하면 토큰 포함 유저 정보를 세션에 넣는다.
         sessionStorage.setItem("pic_isLogined", true);
         sessionStorage.setItem("pic_token", result.user.token);
         sessionStorage.setItem("pic_accountName", result.user.accountname);

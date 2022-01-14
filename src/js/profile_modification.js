7409df1a81d9ecff84c942752e8920426c70e2a8
HanMinSS <gksals6464@naver.com>
2022-01-14 17:49:10 +0900

    #34 ✅  프로필 수정 페이지 js 구현 완료

--- src/js/profile_modification.js
+++ src/js/profile_modification.js
@@ -1,3 +1,10 @@
+const isLogined = sessionStorage.pic_isLogined;
+
+// 회원 서비스 페이지에 비 로그인 상태로 접근할 경우 splash로 이동
+if (!isLogined) {
+  location.href = "./index.html";
+}
+
 const profileImg = document.querySelector(".img_profile");
 const userName = document.querySelector("#userName");
 const userID = document.querySelector("#userID");
@@ -7,8 +14,8 @@ const err_ID = document.querySelector("#userIDError");
 const err_Desc = document.querySelector("#userDescError");
 const submitBtn = document.querySelector(".btn_upload");
 const imgInput = document.querySelector("#img_profile");
-let registerImgUrl = "1641803765586.png"; // 이미지를 업로드 하지 않을 경우 기본이미지가 db에 들어간다.
 
+let registerImgUrl = sessionStorage.getItem("pic_userImg"); // 이미지를 업로드 하지 않을 경우 기존이미지가 db에 들어간다.
 const token = sessionStorage.getItem("pic_token");
 const sessionAccountName = sessionStorage.getItem("pic_accountName");
 
@@ -102,6 +109,7 @@ const handleCheckUserDesc = () => {
 
 // input tag를  blur,input 시 실행
 const handleCheckInput = () => {
+  console.log("good");
   function check() {
     // 유효성 검사를 모두 통과하면 button을 활성화 시킨다.
     if (checkName() && checkID() && checkDesc() && validID) {
@@ -124,37 +132,39 @@ const setProfile = (event) => {
     profileImg.setAttribute("src", event.target.result);
   };
   reader.readAsDataURL(event.target.files[0]);
-  imgUpload();
 };
 
 // 이미지 db에 업로드 후 로그인 시 이용할 변수에 파일이름 할당.
 const imgUpload = () => {
-  const formdata = new FormData();
-  formdata.append("image", imgInput.files[0], "basic-profile-img.png");
-  const requestOptions = {
-    method: "POST",
-    body: formdata,
-    redirect: "follow",
-  };
-  async function fetcher() {
-    return (
-      await fetch("http://146.56.183.55:5050/image/uploadfile", requestOptions)
-    ).json();
+  if (imgInput.files.length) {
+    const formdata = new FormData();
+    formdata.append("image", imgInput.files[0], "basic-profile-img.png");
+    const requestOptions = {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
+    };
+    async function fetcher() {
+      return (
+        await fetch(
+          "http://146.56.183.55:5050/image/uploadfile",
+          requestOptions
+        )
+      ).json();
+    }
+    fetcher().then((value) => {
+      registerImgUrl = value.filename;
+    });
   }
-  fetcher().then((value) => {
-    registerImgUrl = value.filename;
-  });
 };
 
-// 버튼이 활성화되면 버튼에 생기는 onClick event
-const handleOnSubmit = () => {
+const saveModification = () => {
   const myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer " + token);
   myHeaders.append("Content-Type", "application/json");
 
   const raw = JSON.stringify({
     user: {
-      email: email,
-      password: pwd,
       username: userName.value,
       accountname: userID.value,
       intro: userDesc.value,
@@ -163,7 +173,7 @@ const handleOnSubmit = () => {
   });
 
   const requestOptions = {
-    method: "POST",
+    method: "PUT",
     headers: myHeaders,
     body: raw,
     redirect: "follow",
@@ -171,14 +181,22 @@ const handleOnSubmit = () => {
 
   fetch("http://146.56.183.55:5050/user", requestOptions)
     .then((response) => response.json())
-    .then((result) => console.log(result))
-    .then(() => {
-      // 회원가입 완료 쿠키삭제 후 로그인 페이지로 라우팅
-      document.cookie = "EMAIL=; expires=Thu, 01 Jan 1999 00:00:10 GMT;";
-      document.cookie = "PWD=; expires=Thu, 01 Jan 1999 00:00:10 GMT;";
-      location.href = "./login_email.html";
+    .then((result) => {
+      if (!result.message) {
+        alert("프로필이 성공정으로 수정되었습니다.");
+        sessionStorage.setItem("pic_accountName", result.user.accountname);
+        sessionStorage.setItem("pic_userName", result.user.username);
+        sessionStorage.setItem("pic_userImg", result.user.image);
+        location.reload();
+      }
     })
     .catch((error) => console.log("error", error));
+};
+
+// 버튼이 활성화되면 버튼에 생기는 onClick event
+const handleOnSubmit = () => {
+  imgUpload();
+  setTimeout(saveModification, 100);
 };
 
 userName.addEventListener("blur", handleCheckUserName);
@@ -193,3 +211,5 @@ userDesc.addEventListener("blur", handleCheckInput);
 userName.addEventListener("input", handleCheckInput);
 userDesc.addEventListener("input", handleCheckInput);
 userID.addEventListener("input", handleCheckInput);
+
+imgInput.addEventListener("change", handleCheckInput);

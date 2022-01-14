805bd15727429ce5e6a9033c3f2491d61808632f
HanMinSS <gksals6464@naver.com>
2022-01-14 17:00:57 +0900

    #34 💄  프로필 수정 페이지 마크업, 스타일

---
+++ src/js/profile_modification.js
@@ -0,0 +1,160 @@
+let profileImg = document.querySelector(".img_profile");
+let userName = document.querySelector("#userName");
+let userID = document.querySelector("#userID");
+let userDesc = document.querySelector("#userDesc");
+let err_userName = document.querySelector("#userNameError");
+let err_ID = document.querySelector("#userIDError");
+let err_Desc = document.querySelector("#userDescError");
+let submitBtn = document.querySelector(".btn_submit");
+let imgInput = document.querySelector("#img_profile");
+let registerImgUrl = "1641803765586.png";
+// 이미지를 업로드 하지 않을 경우 기본이미지가 db에 들어간다.
+let validID = true;
+
+// username의 길이 유효성 검사
+const checkName = () =>
+  userName.value.length < 11 && userName.value.length > 1 ? true : false;
+
+// accountname의 정규표현식 검사
+const checkID = () => {
+  const regExp = /^[a-zA-Z0-9._]*$/;
+  return regExp.test(userID.value) && !!userID.value ? true : false;
+};
+
+// accountname이 중복인지 검사
+const checkIDValid = () => {
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch("http://146.56.183.55:5050/user", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      validID = true;
+      result.map((item) => {
+        if (item.accountname == userID.value) {
+          err_ID.innerHTML = "*이미 존재하는 아이디 입니다.";
+          validID = false;
+        }
+      });
+    })
+    .catch((error) => console.log("error", error));
+};
+
+// intro(자기소개)란이 공백인지 검사
+const checkDesc = () => (userDesc.value.length != 0 ? true : false);
+
+// input blur 시 실행
+const handleCheckUserName = () => {
+  if (checkName()) err_userName.innerHTML = "";
+  else err_userName.innerHTML = "*사용자 이름은 2~10자 이내여야 합니다.";
+};
+
+// input blur 시 실행
+const handleCheckUserID = () => {
+  if (checkID()) {
+    err_ID.innerHTML = "";
+    checkIDValid();
+  } else err_ID.innerHTML = "*영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.";
+};
+
+// input blur 시 실행
+const handleCheckUserDesc = () => {
+  if (checkDesc()) err_Desc.innerHTML = "";
+  else err_Desc.innerHTML = "*자신을 소개해 주세요.";
+};
+
+// input tag를  blur,input 시 실행
+const handleCheckInput = () => {
+  function check() {
+    // 유효성 검사를 모두 통과하면 button을 활성화 시킨다.
+    if (checkName() && checkID() && checkDesc() && validID) {
+      submitBtn.removeAttribute("disabled");
+      submitBtn.className = "btn_submit activate";
+    } else {
+      submitBtn.setAttribute("disabled", true);
+      submitBtn.className = "btn_submit";
+    }
+  }
+  // fetch 함수의 비동기 처리때문에 setTimeout으로 .1s 후 실행
+  setTimeout(check, 100);
+};
+
+// 이미지 업로드 시 미리보기 view와 이미지 업로드 실행
+const setProfile = (event) => {
+  let reader = new FileReader();
+
+  reader.onload = (event) => {
+    profileImg.setAttribute("src", event.target.result);
+  };
+  reader.readAsDataURL(event.target.files[0]);
+  imgUpload();
+};
+
+// 이미지 db에 업로드 후 로그인 시 이용할 변수에 파일이름 할당.
+const imgUpload = () => {
+  const formdata = new FormData();
+  formdata.append("image", imgInput.files[0], "basic-profile-img.png");
+  const requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
+  async function fetcher() {
+    return (
+      await fetch("http://146.56.183.55:5050/image/uploadfile", requestOptions)
+    ).json();
+  }
+  fetcher().then((value) => {
+    registerImgUrl = value.filename;
+  });
+};
+
+// 버튼이 활성화되면 버튼에 생기는 onClick event
+const handleOnSubmit = () => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    user: {
+      email: email,
+      password: pwd,
+      username: userName.value,
+      accountname: userID.value,
+      intro: userDesc.value,
+      image: registerImgUrl,
+    },
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("http://146.56.183.55:5050/user", requestOptions)
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .then(() => {
+      // 회원가입 완료 쿠키삭제 후 로그인 페이지로 라우팅
+      document.cookie = "EMAIL=; expires=Thu, 01 Jan 1999 00:00:10 GMT;";
+      document.cookie = "PWD=; expires=Thu, 01 Jan 1999 00:00:10 GMT;";
+      location.href = "./login_email.html";
+    })
+    .catch((error) => console.log("error", error));
+};
+
+userName.addEventListener("blur", handleCheckUserName);
+userID.addEventListener("blur", handleCheckUserID);
+userDesc.addEventListener("blur", handleCheckUserDesc);
+
+// input이 아닌 데이터 변경점이 있기 때문에 blur도 추가하였음.
+userName.addEventListener("blur", handleCheckInput);
+userID.addEventListener("blur", handleCheckInput);
+userDesc.addEventListener("blur", handleCheckInput);
+
+userName.addEventListener("input", handleCheckInput);
+userDesc.addEventListener("input", handleCheckInput);
+userID.addEventListener("input", handleCheckInput);

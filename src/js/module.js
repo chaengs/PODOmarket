fddcd6433e18d7cb2fd54b6a3b9ce0b1d6c8d36f
chaeng <cathyshim222@naver.com>
2022-02-16 17:20:51 +0900

    #70♻️ 독바 수정

--- src/js/module.js
+++ src/js/module.js
@@ -2,8 +2,8 @@
 const goBack = () => {
     window.history.back();
 }
-const btnBack = document.querySelector(".btn-back");
-btnBack.addEventListener("click", goBack);
+document.querySelector(".btn-back").addEventListener("click", goBack);
+// btnBack.addEventListener("click", goBack);
 
 // 독바 아이콘 이동
 const goHome = () => {
